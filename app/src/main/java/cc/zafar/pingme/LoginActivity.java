3c9031256f2178f933da12db534946caa9340d64
Zafar Takhirov <cc.rafaz@zafar.cc>
2015-04-16 01:15:57 -0700

    Initial commit

---
+++ app/src/main/java/cc/zafar/pingme/LoginActivity.java
@@ -0,0 +1,144 @@
+package cc.zafar.pingme;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+
+public class LoginActivity extends ActionBarActivity {
+
+	EditText emailEditText, passwordEditText, passwordConfirmEditText;
+	Button loginButton, registerButton;
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+
+		emailEditText = (EditText) findViewById(R.id.email_edit_text);
+		passwordEditText = (EditText) findViewById(R.id.password_edit_text);
+		passwordConfirmEditText = (EditText) findViewById(R.id.password_confirm_edit_text);
+
+		loginButton = (Button) findViewById(R.id.login_button);
+		registerButton = (Button) findViewById(R.id.register_button);
+
+		init();
+
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				parseLogin(emailEditText.getText().toString(),
+				           passwordEditText.getText().toString());
+			}
+		});
+
+		registerButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				parseRegister();
+			}
+		});
+	}
+
+	public void init() {
+		checkIfLoggedIn();
+		passwordConfirmEditText.setVisibility(View.INVISIBLE);
+	}
+
+	private void checkIfLoggedIn() {
+		ParseUser currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+			// TODO: switch to another activity
+		} else {
+			// TODO: show the signup or login screen
+		}
+	}
+
+	private void parseLogin(String email, String password) {
+		ParseUser.logInInBackground(email, password, new LogInCallback() {
+			public void done(ParseUser user, ParseException e) {
+				if (user != null) {
+					// TODO: Hooray! The user is logged in.
+					Toast.makeText(getApplicationContext(),
+					               "Logged in!",
+					               Toast.LENGTH_LONG).show();
+				} else {
+					// TODO: Login failed. Look at the ParseException to see what happened.
+					Toast.makeText(getApplicationContext(),
+					               e.getMessage(),
+					               Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+	}
+
+	private void parseRegister(){
+		if (passwordConfirmEditText.getVisibility() != View.VISIBLE) {
+			passwordConfirmEditText.setVisibility(View.VISIBLE);
+		} else {
+			if (passwordEditText.getText().toString()
+			                    .equals(passwordConfirmEditText.getText().toString())
+				){
+				// TODO: Register with Parse
+				ParseUser user = new ParseUser();
+				user.setUsername(emailEditText.getText().toString());
+				user.setPassword(passwordEditText.getText().toString());
+				user.setEmail(emailEditText.getText().toString());
+
+				// other fields can be set just like with ParseObject
+				// user.put("phone", "650-253-0000");
+
+				user.signUpInBackground(new SignUpCallback() {
+					public void done(ParseException e) {
+						if (e == null) {
+							// TODO: Hooray! Let them use the app now.
+							Toast.makeText(getApplicationContext(),
+							               "Registered!",
+							               Toast.LENGTH_LONG).show();
+						} else {
+							// TODO: Sign up didn't succeed. Look at the ParseException to figure
+							// out what went wrong
+							Toast.makeText(getApplicationContext(),
+							               e.getMessage(),
+							               Toast.LENGTH_LONG).show();
+						}
+					}
+				});
+			} else {
+				Toast.makeText(getApplicationContext(),
+				               getString(R.string.passwords_dont_match),
+				               Toast.LENGTH_LONG).show();
+			}
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu (Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected (MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ app/src/main/java/cc/zafar/pingme/MainApplication.java
@@ -0,0 +1,27 @@
+package cc.zafar.pingme;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+/**
+ * Created by ZafarTakhirov on 4/15/15.
+ */
+public class MainApplication extends Application{
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Initialize Parse:
+		parseInit();
+
+	}
+
+	private void parseInit() {
+		// Enable Local Datastore.
+		Parse.enableLocalDatastore(this);
+		Parse.initialize(this, getString(cc.zafar.pingme.R.string.APP_ID),
+		                 getString(cc.zafar.pingme.R.string.CLIENT_KEY));
+	}
+}
